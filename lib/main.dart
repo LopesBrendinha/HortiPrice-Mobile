3e473219d46254debf2f63745779633c523a3fc7
lopesBrendinha <brendalopes.unicentro@gmail.com>
2025-04-10 13:57:02 -0300

    finalizada relatorios

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginPage(),
+      home: RelatoriosPage(),
       initialRoute: "/",
       routes: {
         "/explicacao": (context) => ExplicacaoPage(),
--- lib/pages/dashboard/relatorios/relatorios_page.dart
+++ lib/pages/dashboard/relatorios/relatorios_page.dart
@@ -10,6 +10,370 @@ class RelatoriosPage extends StatefulWidget {
 class _RelatoriosPageState extends State<RelatoriosPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        backgroundColor: Color.fromRGBO(242, 242, 247, 1),
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(242, 242, 247, 1),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.popAndPushNamed(context, "/home");
+                },
+                child: Image.asset(
+                  './assets/imgs/logo.png',
+                  fit: BoxFit.contain,
+                  height: 240 * 0.45,
+                  width: 290 * 0.45,
+                ),
+              ),
+            ],
+          ),
+          centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: [
+                  SizedBox(width: 15),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Image.asset(
+                        './assets/imgs/pessoa3.png',
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Carlos, qual',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Color.fromRGBO(36, 138, 61, 1),
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Text(
+                        'Relatorio você',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Color.fromRGBO(36, 138, 61, 1),
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Text(
+                        'precisa?',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Color.fromRGBO(36, 138, 61, 1),
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Container(
+                width: 430.0,
+                height: 700,
+                decoration: BoxDecoration(
+                    gradient: const LinearGradient(colors: <Color>[
+                      Color.fromRGBO(48, 219, 91, 1),
+                      Color.fromRGBO(36, 138, 92, 1)
+                    ]),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40),
+                    )),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Color.fromRGBO(242, 242, 247, 1),
+                              shadowColor: Colors.transparent,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                            child: Container(
+                              width: 290,
+                              height: 130,
+                              child: Row(
+                                children: [
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Image.asset(
+                                        './assets/imgs/icon10.png',
+                                        height: 120,
+                                        width: 120,
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(width: 10),
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      ShaderMask(
+                                        shaderCallback: (bounds) =>
+                                            const LinearGradient(
+                                          colors: <Color>[
+                                            Color.fromRGBO(48, 219, 91, 1),
+                                            Color.fromRGBO(36, 138, 92, 1),
+                                          ],
+                                        ).createShader(bounds),
+                                        child: Text(
+                                          'Custeio - ABC',
+                                          style: TextStyle(
+                                            fontSize: 22,
+                                            fontWeight: FontWeight.w700,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 25),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Color.fromRGBO(242, 242, 247, 1),
+                              shadowColor: Colors.transparent,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                            child: Container(
+                              width: 290,
+                              height: 130,
+                              child: Row(
+                                children: [
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Image.asset(
+                                        './assets/imgs/icon9.png',
+                                        height: 100,
+                                        width: 100,
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(width: 33),
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      ShaderMask(
+                                        shaderCallback: (bounds) =>
+                                            const LinearGradient(
+                                          colors: <Color>[
+                                            Color.fromRGBO(48, 219, 91, 1),
+                                            Color.fromRGBO(36, 138, 92, 1),
+                                          ],
+                                        ).createShader(bounds),
+                                        child: Text(
+                                          'Custo',
+                                          style: TextStyle(
+                                            fontSize: 23,
+                                            fontWeight: FontWeight.w700,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                      ShaderMask(
+                                        shaderCallback: (bounds) =>
+                                            const LinearGradient(
+                                          colors: <Color>[
+                                            Color.fromRGBO(48, 219, 91, 1),
+                                            Color.fromRGBO(36, 138, 92, 1),
+                                          ],
+                                        ).createShader(bounds),
+                                        child: Text(
+                                          'Unitario',
+                                          style: TextStyle(
+                                            fontSize: 23,
+                                            fontWeight: FontWeight.w700,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 25),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Color.fromRGBO(242, 242, 247, 1),
+                              shadowColor: Colors.transparent,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                            child: Container(
+                              width: 290,
+                              height: 130,
+                              child: Row(
+                                children: [
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Image.asset(
+                                        './assets/imgs/icon8.png',
+                                        height: 100,
+                                        width: 100,
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(width: 33),
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      ShaderMask(
+                                        shaderCallback: (bounds) =>
+                                            const LinearGradient(
+                                          colors: <Color>[
+                                            Color.fromRGBO(48, 219, 91, 1),
+                                            Color.fromRGBO(36, 138, 92, 1),
+                                          ],
+                                        ).createShader(bounds),
+                                        child: Text(
+                                          'Custo de',
+                                          style: TextStyle(
+                                            fontSize: 23,
+                                            fontWeight: FontWeight.w700,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                      ShaderMask(
+                                        shaderCallback: (bounds) =>
+                                            const LinearGradient(
+                                          colors: <Color>[
+                                            Color.fromRGBO(48, 219, 91, 1),
+                                            Color.fromRGBO(36, 138, 92, 1),
+                                          ],
+                                        ).createShader(bounds),
+                                        child: Text(
+                                          'Produção',
+                                          style: TextStyle(
+                                            fontSize: 23,
+                                            fontWeight: FontWeight.w700,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 25),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Color.fromRGBO(242, 242, 247, 1),
+                              shadowColor: Colors.transparent,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                            child: Container(
+                              width: 290,
+                              height: 130,
+                              child: Row(
+                                children: [
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Image.asset(
+                                        './assets/imgs/icon7.png',
+                                        height: 100,
+                                        width: 100,
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(width: 33),
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      ShaderMask(
+                                        shaderCallback: (bounds) =>
+                                            const LinearGradient(
+                                          colors: <Color>[
+                                            Color.fromRGBO(48, 219, 91, 1),
+                                            Color.fromRGBO(36, 138, 92, 1),
+                                          ],
+                                        ).createShader(bounds),
+                                        child: Text(
+                                          'Total ',
+                                          style: TextStyle(
+                                            fontSize: 23,
+                                            fontWeight: FontWeight.w700,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                      ShaderMask(
+                                        shaderCallback: (bounds) =>
+                                            const LinearGradient(
+                                          colors: <Color>[
+                                            Color.fromRGBO(48, 219, 91, 1),
+                                            Color.fromRGBO(36, 138, 92, 1),
+                                          ],
+                                        ).createShader(bounds),
+                                        child: Text(
+                                          'por Custo',
+                                          style: TextStyle(
+                                            fontSize: 23,
+                                            fontWeight: FontWeight.w700,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ]),
+              ),
+            ],
+          ),
+        ));
   }
 }
