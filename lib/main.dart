2e8fafe2bd5feb0da7a0925e6b1d94f5a26bb541
lopesBrendinha <brendalopes.unicentro@gmail.com>
2025-04-10 20:23:47 -0300

    `feat: Added custom navigation bar to multiple pages and updated bottom navigation bar to use custom navigation bar`

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: RelatoriosPage(),
+      home: SimulacoesPage(),
       initialRoute: "/",
       routes: {
         "/explicacao": (context) => ExplicacaoPage(),
